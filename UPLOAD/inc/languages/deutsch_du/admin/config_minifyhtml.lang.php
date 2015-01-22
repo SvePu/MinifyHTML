4049ac2e43710ee980721ecc2811986443da7e2c
SvePu <svp1702@freenet.de>
2015-01-22 15:00:39 +0100

    Added changed language files for v1.2 #4

--- UPLOAD/inc/languages/deutsch_du/admin/config_minifyhtml.lang.php
+++ UPLOAD/inc/languages/deutsch_du/admin/config_minifyhtml.lang.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* MinifyHTML - 1.0 Language File German (informal)
+* MinifyHTML - 1.2 Language File German (informal)
 * Licensed under GNU/GPL v3
 */
 
@@ -11,5 +11,7 @@ $l['minifyhtml_settings_title_desc'] = "Einstellungen des MinifyHTML-Plugins";
 $l['minifyhtml_enable_title'] = "MinifyHTML-Plugin aktivieren?";
 $l['minifyhtml_enable_title_desc'] = "Wähle JA um die Funktion von MinifyHTML einzuschalten!";
 $l['minifyhtml_limit_title'] = "Renderbegrenzung der Seite";
-$l['minifyhtml_limit_title_desc'] = "Gib die max. Anzahl der Zeichen an, bis zu der der Quelltext gerendert werden soll. - Bei größeren Seiten könnte das Rendern zu längeren Ladezeiten führen. (default: 700000)";
+$l['minifyhtml_limit_title_desc'] = "Gib die max. Anzahl der Zeichen an, bis zu der der Quelltext gerendert werden soll. - Bei größeren Seiten könnte das Rendern zu längeren Ladezeiten führen. (Standard: 700000)";
+$l['minifyhtml_exclpage_title'] = "Seiten von der Minimierung ausschließen";
+$l['minifyhtml_exclpage_title_desc'] = "Gib die Seiten, getrennt durch Komma, an die von der Minimierung ausgeschlossen werden sollen. (z.B.: index.php, portal.php - Standard: leer)";
 ?>
--- UPLOAD/inc/languages/english/admin/config_minifyhtml.lang.php
+++ UPLOAD/inc/languages/english/admin/config_minifyhtml.lang.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* MinifyHTML - 1.0 Language File English
+* MinifyHTML - 1.2 Language File English
 * Licensed under GNU/GPL v3
 */
 
@@ -12,4 +12,6 @@ $l['minifyhtml_enable_title'] = "Activate MinifyHTML plugin?";
 $l['minifyhtml_enable_title_desc'] = "Choose YES to enable plugin function!";
 $l['minifyhtml_limit_title'] = "Site rendering limit";
 $l['minifyhtml_limit_title_desc'] = "Set up maximum count of site rendering characters. - It is possible that this plugin prolongs the opening times of extremely big sites. (default: 700000)";
+$l['minifyhtml_exclpage_title'] = "Exclude pages from minifying";
+$l['minifyhtml_exclpage_title_desc'] = "Set the pages you want exclude from minifying, separated by comma. (e.g.: index.php, portal.php - default: blank)";
 ?>
