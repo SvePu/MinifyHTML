78e04206a82c65d124c877b9e1153e0155fbb020
SvePu <svp1702@freenet.de>
2015-01-22 14:51:38 +0100

    Updated to version 1.2 and added function #4

--- UPLOAD/inc/plugins/minifyhtml.php
+++ UPLOAD/inc/plugins/minifyhtml.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-MinifyHTML Plugin for MyBB
+MinifyHTML Plugin v 1.2 for MyBB
 Copyright (C) 2014 SvePu
 
 This program is free software: you can redistribute it and/or modify
@@ -27,19 +27,45 @@ $plugins->add_hook("pre_output_page","minifyhtml");
 
 function minifyhtml_info()
 {
-	global $lang;
+	global $plugins_cache, $mybb, $db, $lang;
 	$lang->load('config_minifyhtml');
-	return array
+	$info = array
 	(
 		"name"			=>	$db->escape_string($lang->minifyhtml),
 		"description"	=>	$db->escape_string($lang->minifyhtml_desc),
-		"website"		=>	"http://svepu.bplaced.net/minifyhtml-plugin-fuer-mybb/",
+		"website"		=>	"https://github.com/SvePu/MinifyHTML",
 		"author"		=>	"SvePu",
-		"authorsite"	=> 	"http://svepu.bplaced.net",
-		"version"		=>	"1.1",
+		"authorsite"	=> 	"https://github.com/SvePu",
+		"codename"		=>	"minifyhtml",
+		"version"		=>	"1.2",
 		"guid"			=>	"",
 		"compatibility"	=>	"16*,18*"
 	);
+	
+	$info_desc = '';
+	$gid_result = $db->simple_select('settinggroups', 'gid', "name = 'minifyhtml_settings'", array('limit' => 1));
+	$settings_group = $db->fetch_array($gid_result);
+	if(!empty($settings_group['gid']))
+	{
+		$info_desc .= "<span style=\"font-size: 0.9em;\">(~<a href=\"index.php?module=config-settings&action=change&gid=".$settings_group['gid']."\"> ".$db->escape_string($lang->minifyhtml_settings_title)." </a>~)</span>";
+	}
+    
+    if(is_array($plugins_cache) && is_array($plugins_cache['active']) && $plugins_cache['active']['minifyhtml'])
+    {
+		$info_desc .= '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float: right;" target="_blank" />
+<input type="hidden" name="cmd" value="_s-xclick" />
+<input type="hidden" name="hosted_button_id" value="VGQ4ZDT8M7WS2" />
+<input type="image" src="https://www.paypalobjects.com/webstatic/en_US/btn/btn_donate_pp_142x27.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1" />
+</form>';
+	}
+	
+	if($info_desc != '')
+	{
+		$info['description'] = $info_desc.'<br />'.$info['description'];
+	}
+    
+    return $info;
 }
 
 function minifyhtml_activate()
@@ -75,12 +101,23 @@ function minifyhtml_activate()
 		"name"			=> "minifyhtml_limit",
 		"title"			=> $db->escape_string($lang->minifyhtml_limit_title),
 		"description" 	=> $db->escape_string($lang->minifyhtml_limit_title_desc),
-        'optionscode'  	=> 'text',
+        'optionscode'  	=> 'numeric',
         'value'        	=> '700000',
 		"disporder"		=> "2",
 		"gid" 			=> (int)$gid
 	);
 	$db->insert_query("settings", $minifyhtml_2);
+	
+	 $minifyhtml_3 = array(
+		"name"			=> "minifyhtml_exclpage",
+		"title"			=> $db->escape_string($lang->minifyhtml_exclpage_title),
+		"description" 	=> $db->escape_string($lang->minifyhtml_exclpage_title_desc),
+        'optionscode'  	=> 'text',
+        'value'        	=> '',
+		"disporder"		=> "3",
+		"gid" 			=> (int)$gid
+	);
+	$db->insert_query("settings", $minifyhtml_3);
 	rebuild_settings();
 }
 
@@ -102,13 +139,19 @@ function minifyhtml_deactivate()
 function minifyhtml($page)
 {
 	global $mybb;
-	if (empty($mybb->settings['minifyhtml_limit'])){$mybb->settings['minifyhtml_limit'] = 700000;}
 	if ($mybb->settings['minifyhtml_enable'] == 1){
-		if (strlen($page) > $mybb->settings['minifyhtml_limit']){return $page;}
+		if ($mybb->settings['minifyhtml_limit'] <= 0){
+			$mybb->settings['minifyhtml_limit'] = 700000;
+		}
+		if ((strlen($page) > $mybb->settings['minifyhtml_limit']) || (strpos($mybb->settings['minifyhtml_exclpage'], THIS_SCRIPT) !== false)){
+			return $page;
+		}
 		$ignore_tags = array('textarea','pre','script');
 		$ignore_regex = implode('|', $ignore_tags);
 		$cleaned_page = preg_replace(array('/(?:^\\s*<!--\\s*|\\s*(?:\\/\\/)?\\s*-->\\s*$)/','#(?ix)(?>[^\S ]\s*|\s{2,})(?=(?:(?:[^<]++|<(?!/?(?:' .$ignore_regex. ')\b))*+)(?:<(?>' .$ignore_regex. ')\b|\z))#'),array('',' '),$page);
-		if ( strlen($cleaned_page) <= 1 ) {return $page;}
+		if ( strlen($cleaned_page) <= 1 ) {
+			return $page;
+		}
 		return $cleaned_page;
 	}
 }
